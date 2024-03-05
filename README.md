3d8ea963e1c088a9ef389750353bcfbba96c786f
RAIMUNDO GOMES RESENDE NETO <RAIMUNDO0423@DIGITALCOLLEGE.LOCAL>
2024-03-05 20:04:18 -0300

    subindo get e post

---
+++ README.md
@@ -0,0 +1,25 @@
+insomnia
+netoresende123
+
+
+## exemplo do post 
+
+```json
+[
+	{
+		"id": 1,
+		"nome": "maçã",
+		"preco": 5.7,
+		"desconto": 5,
+		"estoque": false
+	},
+	{
+		"id": 2,
+		"nome": "pera",
+		"preco": 7.7,
+		"desconto": 5,
+		"estoque": false
+	}
+]
+
+```
--- server.js
+++ server.js
@@ -1,24 +1,58 @@
- import Fastify from 'fastify'
+// doc do fastify para subir o servidor: https://fastify.dev/docs/latest/Guides/Getting-Started/
+import Fastify from "fastify";
+import { produtos } from "./db/produtos.js";
 
- const PORT = 5005
- const HOST = 'localhost'
- 
-  // criando o object fastify a partir da classe fastify que foi importada
- const fastify = Fastify({
-    logger: true
- })
+const PORT = 5005;
+const HOST = "localhost";
 
- // declarando a primeira rota, neste caso, um get a partir do objeto fastify instanciado na linha 5
- fastify.get('/', ( req, res ) =>{
-    res.send('Servidor rodando')
- })
+function buscarProdutoPorId(id){
+   let produto = produtos.filter(produto => produto.id == id )
+   return produto
+}
 
- // o metodo listen()  é responsável por subir o servidor
-fastify.listen({port: PORT, host: HOST}, (error, address )=>{
-   if(error){
-      console.error(`Erro ao subir o servidor: ${error}`);
-      return;
-   }
-   console.log(`Servidor rodando em http://${HOST}:${PORT}`);
-})
+// criando o object fastify a partir da classe fastify que foi importada
+const fastify = Fastify({
+  logger: true,
+});
 
+// declarando a primeira rota, neste caso, um get a partir do objeto fastify instanciado na linha 5
+fastify.get("/", (req, res) => {
+  res.send("Servidor rodando");
+});
+
+
+fastify.get("/produtos", (req, res) => {
+   res.send({
+      qtd: produtos.length,
+      data: produtos
+   })
+});
+
+fastify.get("/produto/:id", (req, res) => {
+   // let produto = buscarProdutoPorId(req.params.id)
+   res.send(buscarProdutoPorId(req.params.id))
+   
+});
+
+
+fastify.post("/produto", (req, res) => {
+  const { nome, preco, desconto, estoque } = req.body;
+  const novoProduto = {
+    id: produtos.length + 1,
+    nome,
+    preco,
+    desconto,
+    estoque,
+  };
+  produtos.push(novoProduto);
+  res.send("Produto cadastrado com sucesso!");
+});
+
+// o metodo listen()  é responsável por subir o servidor
+fastify.listen({ port: PORT, host: HOST }, (error, address) => {
+  if (error) {
+    console.error(`Erro ao subir o servidor: ${error}`);
+    return;
+  }
+  console.log(`Servidor rodando em http://${HOST}:${PORT}`);
+});
